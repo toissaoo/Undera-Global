d9eb29a1c7ef66e8d9b677d55240794cd2e1714f
Underewar <74227915+Underewarrr@users.noreply.github.com>
2021-04-26 10:32:38 -0300

    Create Duke Krule Lever

---
+++ data/scripts/actions/dukes/druke_krule.lua
@@ -0,0 +1,54 @@
+local setting = {
+	centerRoom = {x = 33456, y = 31478, z = 13},
+	storage = Storage.GraveDanger.Dukes.Krule,
+	bossPosition = {x = 33456, y = 31477, z = 13},
+	kickPosition = {x = 33456, y = 31498, z = 13},
+	playerTeleport = {x = 33456, y = 31478, z = 13}
+}
+
+local kruleLever = Action()
+
+-- Start Script
+function kruleLever.onUse(creature, item, fromPosition, target, toPosition, isHotkey)
+	if item.itemid == 9825 and item.actionid == 20004 then
+	local clearOberonRoom = Game.getSpectators(Position(setting.centerRoom), false, false, 10, 10, 10, 10)       
+	for index, spectatorcheckface in ipairs(clearOberonRoom) do
+		if spectatorcheckface:isPlayer() then
+			creature:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Someone is fighting against the boss! You need wait awhile.")
+			return false
+		end
+	end	
+	for index, removeOberon in ipairs(clearOberonRoom) do
+		if (removeOberon:isMonster()) then
+			removeOberon:remove()
+		end
+	end
+		Game.createMonster("Duke Krule", setting.bossPosition, false, true)
+local players = {}
+	for i = 0, 4 do
+		local player1 = Tile({x = (Position(item:getPosition()).x - 2) + i, y = Position(item:getPosition()).y + 1, z = Position(item:getPosition()).z}):getTopCreature()
+		players[#players+1] = player1
+	end
+		for i, player in ipairs(players) do
+			player:getPosition():sendMagicEffect(CONST_ME_POFF)
+			player:teleportTo(Position(setting.playerTeleport), false)
+			doSendMagicEffect(player:getPosition(), CONST_ME_TELEPORT)
+			setPlayerStorageValue(player,setting.storage, os.time() + 20 * 60 * 60)
+            player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'You have 20 minute(s) to defeat the boss.')
+				addEvent(function()
+					local spectatorsOberon = Game.getSpectators(Position(setting.centerRoom), false, false, 10, 10, 10, 10)
+						for u = 1, #spectatorsOberon, 1 do
+							if spectatorsOberon[u]:isPlayer() and (spectatorsOberon[u]:getName() == player:getName()) then
+								player:teleportTo(Position(setting.kickPosition))
+								player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+								player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Time is over.')
+							end
+						end
+				end, 20 * 60 * 1000)
+		end
+	end
+	return true
+end
+
+kruleLever:aid(20004)
+kruleLever:register()
