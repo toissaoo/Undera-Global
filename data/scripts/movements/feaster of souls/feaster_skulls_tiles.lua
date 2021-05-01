69ba64705f8c5d7d7925a5d6598a6dcca708d9d0
Underewar <74227915+Underewarrr@users.noreply.github.com>
2021-05-01 12:22:47 -0300

    feaster Psoul PH

--- data/scripts/movements/feaster of souls/feaster_skulls_tiles.lua
+++ data/scripts/movements/feaster of souls/feaster_skulls_tiles.lua
@@ -11,7 +11,11 @@ local config = {
 	[30011] = Position(33571, 31467, 9),
     [30012] = Position(33557, 31467, 9),
 		-- barren entrance --
-	[30013] = Position{33220, 31704, 7}
+	[30013] = Position{33220, 31704, 7},
+		-- TPS VENGOTH --
+	[14530] = Position(33648, 31445, 10),
+	[14531] = Position(33648, 31445, 10)
+	
 }
 
 local variavelName = MoveEvent()
